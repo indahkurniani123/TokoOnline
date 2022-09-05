64ee1fd2c3396b6bc3144f29a9e4e27f0c7a8813
20310004 <indah_20310004@stimata.ac.id>
2022-09-05 15:30:59 +0700

    project new

---
+++ resources/views/hallo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hallo</title>
+</head>
+<body>
+    <h2>Wellcome to My Matakulia</h2>
+    <h3> {{ $nama }} </h3>
+</body>
+</html>
